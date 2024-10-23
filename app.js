863a043e375705f6fedefcec332171c317f013b0
Jeeva <48169919+Jeeva-ss@users.noreply.github.com>
2024-10-23 14:16:43 +0530

    UI Updated

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const nav = document.querySelector('.nav-links');
 const burger = document.querySelector('.burger');
 const links = nav.querySelectorAll('a')
 
+const currentYear1 = document.querySelector(".footer-wrapper h4 span");
+const currentYear2 = new Date().getFullYear();
+currentYear1.innerText = currentYear2;
+
 const handleClick = () => {
   nav.classList.toggle('nav-open')
   burger.classList.toggle('toggle')
